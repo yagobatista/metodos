e8ebc249f3825002a1b912446c5172b4bb699da2
yagobatista <yagobatistasilva@gmail.com>
2017-11-07 00:38:23 -0200

    finalizou metodo da secante

--- metodos-trab1/js/script.js
+++ metodos-trab1/js/script.js
@@ -1,22 +1,36 @@
-function secante(coe, max) {
+function secante(max, precisao) {
   //coe array
-  coe[0] = 1;
-  coe[1] = 4;
-  coe[2] = 5;
-  coe[3] = 7;
-  coe[4] = 3;
-  coe[5] = 4;
-  valor[0] = 0;
-  valor[1] = 1;
-  for (var i = 2; i < max; i++) {
-    valor[i]
+  var coe = new Array();
+  coe[0] = 3;
+  coe[1] = -9;
+  coe[2] = 0;
+  coe[3] = 1;
+  coe[4] = 0;
+  coe[5] = 0;
+  var valor = new Array();
+  valor[1] = 0;
+  valor[2] = 1;
+  var string = "x1 = " + valor[1] + " -> f(x) = " + montar(coe, valor[1]) + "<br>";
+  string += "x2 = " + valor[2] + " -> f(x) = " + montar(coe, valor[2]) + "<br>";
+  max += 2;
+  for (var i = 3; i <= max && modulo(montar(coe, valor[i - 1])) > precisao; i++) {
+    valor[i] = valor[i - 1] - (montar(coe, valor[i - 1])) * ((valor[i - 1] - valor[i - 2]) / (montar(coe, valor[i - 1]) - montar(coe, valor[i - 2])));
+    string += "x" + i + " =" + valor[i] + " -> f(x) = " + montar(coe, valor[i]) + "<br>";
   }
+  document.getElementById('resultado').innerHTML = string;
 }
 
 function montar(coe, x) {
-  var resultado;
+  var resultado = 0;
   for (var i = 0; i < coe.length; i++) {
-    resultado += coe[i] * math.pow(x, i);
+    resultado = resultado + coe[i] * Math.pow(x, i);
   }
   return resultado;
+}
+
+function modulo(valor) {
+  if (valor < 0) {
+    return -1 * valor;
+  }
+  return valor;
 }
